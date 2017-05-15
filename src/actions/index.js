b911e7e243519101425231796e2df4a279d13bde
RX7RUSS <russ.savage5@gmail.com>
2017-05-15 11:47:21 -0500

    Everything except components completed

--- src/actions/index.js
+++ src/actions/index.js
@@ -6,6 +6,12 @@ export function increaseCounter(){
   }
 }
 
+export function decreaseCounter(){
+  return {
+    type:"DECREASE_COUNTER"
+  }
+}
+
 export function setSpecialText(txt){
   return {
     type:"SET_SPECIAL_TEXT",
@@ -19,8 +25,58 @@ export function addUser(user){
     value:user
   }
 }
-export function removeUser(){
+
+export function removeUser(user){
   return {
     type:"REMOVE_USER"
+  }
+}
+
+export function setSearchText(text){
+  return {
+    type:"SET_SEARCH_TEXT",
+    value:text
+  }
+}
+
+export function isLoading(isLoading){
+  return {
+    type:"ADD_USER",
+    value:isLoading
+  }
+}
+
+export function setTemp(temp){
+  return {
+    type:"SET_TEMP",
+    value:temp
+  }
+}
+
+export function setCurrentCity(city){
+  return {
+    type:"SET_CURRENT_CITY",
+    value:city
+  }
+}
+
+export function setVideoURL(URL){
+  return {
+    type:"SET_VIDEO_URL",
+    value:URL
+  }
+}
+
+export function setCurrentUserSort(sort){
+  return {
+    type:"SET_CURRENT_USER_SORT",
+    value:sort
+  }
+}
+
+export function setVideoScale(scale){
+  return {
+    type:"SET_VIDEO_SCALE",
+    value:scale
   }
 }
