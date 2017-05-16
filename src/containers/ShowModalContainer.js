61a319f544bf6b810b1a25621de798579b3e1a15
joeycaradonna <joeycaradonna45@gmail.com>
2017-05-15 22:24:20 -0500

    Redux Action Practice

---
+++ src/containers/ShowModalContainer.js
@@ -0,0 +1,15 @@
+import { connect } from "react-redux";
+import {setIsLoading} from "../actions"
+import ShowModal from "../components/ShowModal"
+
+function mapDispatchToProps(dispatch){
+  return{
+    setIsLoading: function(isLoading){
+      let action = setIsLoading(isLoading);
+      dispatch(action);
+    }
+  }
+}
+
+const ShowModalContainer = connect(null, mapDispatchToProps)(ShowModal);
+export default ShowModalContainer
---
+++ src/containers/SpecialTextBoxContainer.js
@@ -0,0 +1,17 @@
+import { connect } from "react-redux";
+import {setSpecialText} from "../actions"
+import SpecialTextBox from "../components/SpecialTextBox"
+
+function mapDispatchToProps(dispatch){
+  return{
+    set: function(txt){
+
+      let action = setSpecialText(txt);
+
+      dispatch(action);
+    }
+  }
+}
+
+const SpecialTextBoxContainer = connect(null, mapDispatchToProps)(SpecialTextBox);
+export default SpecialTextBoxContainer;
