e0c9ac1a407421b090a1958252a003f915a46dbc
stackbaxter <stackbaxter@yahoo.com>
2017-08-12 01:00:10 -0500

    actions done!

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,30 +1,78 @@
 export function increaseCounter(){
   return {
-    type:"INCREASE_COUNTER"
+    type: "INCREASE_COUNTER"
   }
 }
 
 export function decreaseCounter(){
   return {
-    type:"DECREASE_COUNTER"
+    type: "DECREASE_COUNTER"
   }
 }
 
-export function setSpecialText(txt){
+export function setSpecialText(text){
   return {
-    type:"SET_SPECIAL_TEXT",
-    value:text
+    type: "SET_SPECIAL_TEXT",
+    value: text
   }
 }
 
 export function addUser(user){
   return {
-    type:"ADD_USER",
-    value:user
+    type: "ADD_USER",
+    value: user
   }
 }
 export function removeUser(){
   return {
     type:"REMOVE_USER"
+  }
+}
+export function setSearchText(text){
+    return {
+      type: "SET_SEARCH_TEXT",
+      value: text
+    }
+}
+
+export function setIsLoading(isLoading){
+  return {
+    type: "SET_IS_LOADING",
+    value: isLoading
+  }
+}
+
+export function setTemp(temp){
+  return {
+    type: "SET_TEMP",
+    value: temp
+  }
+}
+
+export function setCurrentCity(city){
+  return{
+    type: "SET_CURRENT_CITY",
+    value: city
+  }
+}
+
+export function setVideoURl(URL){
+  return {
+    type: "SET_VIDEO_URL",
+    value: URL
+  }
+}
+
+export function setCurrentUser(sort){
+  return {
+    type: "SET_CURRENT_USER",
+    value: sort
+  }
+}
+
+export function setVideoScale(scale){
+  return {
+    type: "SET_VIDEO_SCALE",
+    value: scale
   }
 }
