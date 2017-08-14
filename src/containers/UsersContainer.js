03dbb083f1696144d85cef44d4adb06b59f2d6c1
stackbaxter <stackbaxter@yahoo.com>
2017-08-14 14:36:09 -0500

    conainer time!

---
+++ src/containers/UsersContainer.js
@@ -0,0 +1,14 @@
+import { connect } from 'react-redux';
+// import {setCurrentUser} from "../actions";
+import Users from "../components/Users";
+
+//map a prop called users to state.users
+function mapStateToProps(state){
+  return {
+    users: state.users,
+    firstNameFilter: state.searchText,
+    sortOn: state.currentUsers.Sort
+  }
+}
+
+export default connect(mapStateToProps)(Users);
