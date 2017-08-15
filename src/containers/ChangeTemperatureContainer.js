02e52f5c7dcb37c0cc92975d45d247dd4e0eb0ad
Mehmet Sonmezates <sonmezatesmehmet@gmail.com>
2017-08-15 13:21:09 -0500

    finish sixth step // create container, import connect and appropriate components

---
+++ src/containers/ChangeTemperatureContainer.js
@@ -0,0 +1,3 @@
+import { connect } from "react-redux";
+import { setTemp } from "../actions";
+import ChangeTemperature from "../components/ChangeTemperature";
---
+++ src/containers/SearchTexBoxContainer.js
@@ -0,0 +1,2 @@
+import { connect } from "react-redux";
+import SearchTextBox from "../components/SearchTextBox";
