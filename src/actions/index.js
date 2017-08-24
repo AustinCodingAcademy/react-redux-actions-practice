5c4efc83c6f4db240362cece041ff0daf7775f52
mymonstermaggie <maggie@remote-com.com>
2017-08-23 19:49:59 -0500

    add actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,24 +1,79 @@
-export function increaseCounter(){
+export function increaseCounter() {
   return {
-    type:"INCREASE_COUNTER"
+    type: "INCREASE_COUNTER"
   }
 }
 
-export function setSpecialText(txt){
+export function decreaseCounter() {
   return {
-    type:"SET_SPECIAL_TEXT",
-    value:txt
+    type: "DECREASE_COUNTER"
   }
 }
 
-export function addUser(user){
+export function setSpecialText(text) {
   return {
-    type:"ADD_USER",
-    value:user
+    type: "SET_SPECIAL_TEXT",
+    value: text
   }
 }
-export function removeUser(){
+
+export function addUser(user) {
   return {
-    type:"REMOVE_USER"
+    type: "ADD_USER",
+    value: user
+  }
+}
+export function removeUser() {
+  return {
+    type: "REMOVE_USER"
+  }
+}
+
+export function setSearchText(text) {
+  return {
+    type: "SET_SEARCH_TEXT",
+    value: text
+  }
+}
+
+export function setIsLoading(isLoading) {
+  return {
+    type: "SET_IS_LOADING",
+    value: isLoading
+  }
+}
+
+export function setTemp(temp) {
+  return {
+    type: "SET_TEMP",
+    value: temp
+  }
+}
+
+export function setCurrentCity(city) {
+  return {
+    type: "SET_CURRENT_CITY",
+    value: city
+  }
+}
+
+export function setVideoURL(URL) {
+  return {
+    type: "SET_VIDEO_URL",
+    value: URL
+  }
+}
+
+export function setCurrentUserSort(sort) {
+  return {
+    type: "SET_CURRENT_USER_SORT",
+    value: sort
+  }
+}
+
+export function setVideoScale(scale) {
+  return {
+    type: "SET_VIDEO_SCALE",
+    value: scale
   }
 }
