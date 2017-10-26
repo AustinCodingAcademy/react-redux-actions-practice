b8a6eca6d074341466ddbfb646779a5448a07eda
CGottschallJ <Cameron.Gottschall@gmail.com>
2017-10-25 20:58:41 -0500

    initial: through store.  begin containers

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,12 +4,19 @@ export function increaseCounter(){
   }
 }
 
+export function decreaseCounter(){
+  return {
+    type:"INCREASE_COUNTER"
+  }
+}
+
 export function setSpecialText(txt){
   return {
     type:"SET_SPECIAL_TEXT",
-    value:txt
+    value: txt
   }
 }
+
 
 export function addUser(user){
   return {
@@ -20,5 +27,54 @@ export function addUser(user){
 export function removeUser(){
   return {
     type:"REMOVE_USER"
+  }
+}
+
+export function setSearchText(txt){
+  return {
+    type:"SET_SEARCH_TEXT",
+    value: txt
+  }
+}
+
+export function setIsLoading(isLoading){
+  return {
+    type:"SET_IS_LOADING",
+    value: isLoading
+  }
+}
+
+export function setTemp(temp){
+  return {
+    type:"SET_TEMP",
+    value: temp
+  }
+}
+
+export function setCurrentCity(city){
+  return {
+    type:"SET_CURRENT_CITY",
+    value: city
+  }
+}
+
+export function setVideoURL(URL){
+  return {
+    type:"SET_VIDEO_URL",
+    value: URL
+  }
+}
+
+export function setCurrentUserSort(sort){
+  return {
+    type:"SET_CURRENT_USER_SORT",
+    value: sort
+  }
+}
+
+export function setVideoScale(scale){
+  return {
+    type:"SET_VIDEO_SCALE",
+    value: scale
   }
 }
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,20 +1,21 @@
+import {combineReducers} from 'redux';
 
 function currentCount(state=0, action){
   if(action.type === "INCREASE_COUNTER"){
-    
+    return currentCount + 1;
   }
   if(action.type === "DECREASE_COUNTER"){
-    
+    return currentCount - 1;
   }
   return state;
-}   
+}
 
 function users(state =[], action){
   if(action.type === "ADD_USER"){
-
+    return [...state,action.value];
   }
   if(action.type === "REMOVE_USER"){
-    
+    return state.slice(1);
   }
   return state;
 }
@@ -26,3 +27,67 @@ function specialText(state = "", action){
   }
   return state;
 }
+
+function currentCity(state = "", action){
+  if(action.type === "SET_CURRENT_CITY"){
+    return action.value;
+  }
+  return state;
+}
+
+function currentTemp(state = 0, action){
+  if(action.type === "SET_TEMP"){
+    return action.value;
+  }
+  return state;
+}
+
+function isLoading(state = false, action){
+  if(action.type === "SET_IS_LOADING"){
+    return action.value;
+  }
+  return state;
+}
+
+function videoURL(state = "", action){
+  if(action.type === "SET_VIDEO_URL"){
+    return action.value;
+  }
+  return state;
+}
+
+function searchText(state = "", action){
+  if(action.type === "SET_SEARCH_TEXT"){
+    return action.value;
+  }
+  return state;
+}
+
+function currentUserSort(state = "", action){
+  if(action.type === "SET_CURRENT_USER_SORT"){
+    return action.value;
+  }
+  return state;
+}
+
+function videoScale(state = 0, action){
+  if(action.type === "SET_VIDEO_SCALE"){
+    return action.value;
+  }
+  return state;
+}
+
+const rootReducer = combineReducers(
+  {
+    currentCount,
+    users,
+    specialText,
+    currentCity,
+    currentTemp,
+    isLoading,
+    videoURL,
+    searchText,
+    currentUserSort,
+    videoScale
+  });
+export default rootReducer;
