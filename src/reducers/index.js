14c16e4ef3ce3f972c62184461639f374b240ead
Andrew Hartford <andrewhartford@utexas.edu>
2017-11-02 23:06:50 -0500

    updates

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,11 @@
 
 function currentCount(state=0, action){
   if(action.type === "INCREASE_COUNTER"){
-    
+    return state + 1;
   }
+
   if(action.type === "DECREASE_COUNTER"){
-    
+    return state - 1;
   }
   return state;
 }   
@@ -22,6 +23,36 @@ function users(state =[], action){
 
 function specialText(state = "", action){
   if(action.type === "SET_SPECIAL_TEXT"){
+    return action.value;
+  }
+  return state;
+}
+
+
+function currentCity(state = "", action){
+  if(action.type === "SET_CURRENT_CITY"){
+    return action.value;
+  }
+  return state;
+}
+
+function searchText(state = "", action){
+  if(action.type === "SET_SEARCH_TEXT"){
+    return action.value;
+  }
+  return state;
+}
+
+
+function currentTemp(state = "", action){
+  if(action.type === "SET_TEMP"){
+    return action.value;
+  }
+  return state;
+}
+
+function isLoading(state = false, action){
+  if(action.type === "SET_IS_LOADING"){
     return action.value;
   }
   return state;
