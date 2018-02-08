dab6072b61fcce90807fa81d01e47e1fa2b46388
conorf <conorfleming@outlook.com>
2018-02-07 19:56:34 -0600

    added actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,14 @@
 export function increaseCounter(){
   return {
-    type:"INCREASE_COUNTER"
+    type:"INCREASE_COUNTER",
+    value: 1
+  }
+}
+
+export function decreaseCounter(){
+  return {
+    type:"DECREASE_COUNTER",
+    value: 1
   }
 }
 
@@ -20,5 +28,54 @@ export function addUser(user){
 export function removeUser(){
   return {
     type:"REMOVE_USER"
+  }
+}
+
+export const setSearchText = (text) => {
+  return {
+    type: "SET_SEARCH_TEXT",
+    value: text
+  }
+}
+
+export const setIsLoading = (isLoading) => {
+  return {
+    type: "SET_IS_LOADING",
+    value: isLoading
+  }
+}
+
+export const setTemp = (temp) => {
+  return {
+    type: "SET_TEMP",
+    value: temp
+  }
+}
+
+export const setCurrentCity = (city) => {
+  return {
+    type: "SET_CURRENT_CITY",
+    value: city
+  }
+}
+
+export const setVideoURL = (URL) => {
+  return {
+    type: "SET_VIDEO_URL",
+    value: URL
+  }
+}
+
+export const setCurrentUserSort = (sort) => {
+  return {
+    type: "SET_CURRENT_USER_SORT",
+    value: sort
+  }
+}
+
+export const setVideoScale = (scale) => {
+  return {
+    type: "SET_VIDEO_SCALE",
+    value: scale
   }
 }
