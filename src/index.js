4f04363111c0def6f98ea0f2ee427323f8bf2d1d
conorf <conorfleming@outlook.com>
2018-02-07 20:12:41 -0600

    added Provider to index

--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+import { Provider } from 'react-redux';
 
 ReactDOM.render(
-  <App />,
+  <Provider store={store}><App /></Provider>,
   document.getElementById('root')
 );
