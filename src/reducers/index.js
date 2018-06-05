b3de211e09a2017e5c8b96dfd62c53a59f62346b
walzer85 <walzer85@gmail.com>
2018-06-05 18:59:11 -0500

    reducers and actions

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -28,8 +28,59 @@ function specialText(state = "", action){
   return state;
 }
 
+function currentCity(state = "", action){
+  if(action.type === "SET_CURRENT_CITY"){
+    return action.value;
+  }
+  return state;
+}
+
+function currentTemp(state = "", action){
+  if(action.type === "SET_TEMP"){
+    return action.value;
+  }
+  return state;
+}
+
+function isLoading(state = "", action){
+  if(action.type === "SET_IS_LOADING"){
+    return action.value;
+  }
+  return state;
+}
+
+function videoUrl(state = "", action){
+  if(action.type === "SET_VIDEO_URL"){
+    return action.value;
+  }
+  return state;
+}
+
+function searhText(state = "", action){
+  if(action.type === "SET_SEARCH_TEXT"){
+    return action.value;
+  }
+  return state;
+}
+
+function currentUserSort(state = "", action){
+  if(action.type === "SET_CURRENT_USER_SORT"){
+    return action.value;
+  }
+  return state;
+}
+
+function videoScale(state = "", action){
+  if(action.type === "SET_VIDEO_SCALE"){
+    return action.value;
+  }
+  return state;
+}
+
+
+
 const rootReducer = combineReducers({
-  currentCount
+  currentCount, user, specialText, currentCity, currentTemp, isLoading, videoUrl, searhText, currentUserSort, videoScale
 })
 
 export default rootReducer;
