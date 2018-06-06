65b446d21d08bfd8bbcdd240efc9296d602154a1
Shelby Inloes <shelbyinloes@gmail.com>
2018-06-06 14:52:43 -0500

    adding comments

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,6 @@
 import { combineReducers } from "redux";
 
-
+//working
 function currentCount(state=0, action){
   if(action.type === "INCREASE_COUNTER"){
     return state + 1; 
@@ -11,14 +11,16 @@ function currentCount(state=0, action){
   return state;
 }   
 
-function users(state =[], action){
+// * users
+// * if "REMOVE_USER" OR "ADD_USER" should return slice(1) 
+// to remove the first user or [...state,action.value] to add a user
+function users(state=[], action){
   if(action.type === "ADD_USER"){
     let addUserArray = [...state];
     addUserArray.push(action.value);
     return addUserArray;
   }
   if(action.type === "REMOVE_USER"){
-    //slice
     let removeUserArray = [...state];
     removeUserArray.slice(action.value);
     return removeUserArray;
@@ -26,7 +28,8 @@ function users(state =[], action){
   return state;
 }
 
-
+// * specialText
+// * if "SET_SPECIAL_TEXT" should return the action value
 function specialText(state = "", action){
   if(action.type === "SET_SPECIAL_TEXT"){
     return action.value;
@@ -34,8 +37,84 @@ function specialText(state = "", action){
   return state;
 }
 
+//name the function equal to the state
+//action type is what is in action folder
+
+// * currentCity
+// * if "SET_CURRENT_CITY" should return the action value
+function currentCity(state="", action){
+  if(action.type === "SET_CURRENT_CITY"){
+    return action.value; 
+  }
+  return state;
+}  
+
+// * searchText
+// * if "SET_SEARCH_TEXT" should return action value
+function searchText(state="", action){
+  if(action.type === "SET_SEARCH_TEXT"){
+    return //something
+  }
+  return state;
+}  
+
+// * currentTemp
+// * if "SET_TEMP" should return action value
+function currentTemp(state=0, action){
+  if(action.type === "SET_TEMP"){
+    return //something
+  }
+  return state;
+}  
+
+// * isLoading
+// * if "SET_IS_LOADING" should return action value 
+function isLoading(state=false, action){
+  if(action.type === "SET_IS_LOADING"){
+    return //something
+  }
+  return state;
+}  
+
+
+// * videoURL
+// * if "SET_VIDEO_URL" should return action value
+function videoURL(state="", action){
+  if(action.type === "SET_VIDEO_URL"){
+    return //something
+  }
+  return state;
+}  
+
+// * currentUserSort
+// * if "SET_CURRENT_USER_SORT" should return action value
+function currentUserSort(state=first_name, action){
+  if(action.type === "SET_CURRENT_USER_SORT"){
+    return //something
+  }
+  return state;
+}  
+
+// * videoScale
+// * if "SET_VIDEO_SCALE" should return action value
+function videoScale(state=1, action){
+  if(action.type === "SET_VIDEO_SCALE"){
+    return //something
+  }
+  return state;
+}  
+
 const rootReducer = combineReducers({
-  currentCount
+  currentCount, 
+  users, 
+  specialText, 
+  currentCity, 
+  searchText, 
+  currentTemp, 
+  isLoading, 
+  videoURL, 
+  currentUserSort, 
+  videoScale
 });
 
 export default rootReducer;
