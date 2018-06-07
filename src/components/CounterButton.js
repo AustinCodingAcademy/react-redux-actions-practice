785009cb3070657fd7a1666a03d42c10714e31bf
Shelby Inloes <shelbyinloes@gmail.com>
2018-06-06 20:50:46 -0500

    taking out redux

--- src/components/CounterButton.js
+++ src/components/CounterButton.js
@@ -1,6 +1,4 @@
 import React from "react";
-import { increaseCounter, decreaseCounter } from "../actions";
-import { connect } from "react-redux";
 
 function CounterButton(props) {
   return (
@@ -23,19 +21,5 @@ function CounterButton(props) {
   );
 }
 
-//  I want to change the data
-//  dispatch is sending this somewhere 
-function mapDispatchToProps(dispatch){
-  return {
-    increase:function(){
-      var action = increaseCounter();
-      dispatch(action);
-    },
-    decrease:function(){
-      var action = decreaseCounter();
-      dispatch(action);
-    }
-  }
-}
 
-export default connect(null,mapDispatchToProps)(CounterButton);
+export default CounterButton;
