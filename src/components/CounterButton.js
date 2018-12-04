37d5f34e2dda4cf11a515f3beb3784d221ab2724
SeanPhilippi <sean.philippi@gmail.com>
2018-12-03 19:47:32 -0600

    fixed reducer

--- src/components/CounterButton.js
+++ src/components/CounterButton.js
@@ -1,23 +1,39 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { increaseCounter, decreaseCounter } from '../actions/index';
 
 function CounterButton(props) {
   return (
-      <div>
-        <button onClick={
-          ()=>{
-            if(props.increase){
-              props.increase();
-            }
+    <div>
+      <button onClick={
+        () => {
+          if (props.increase) {
+            props.increase();
           }
-        }>Increase Counter By One</button>
-        <button onClick={
-          ()=>{
-            if(props.increase){
-              props.decrease();
-            }
+        }
+      }>Increase Counter By One</button>
+      <button onClick={
+        () => {
+          if (props.decrease) {
+            props.decrease();
           }
-        }>Decrease Counter By One</button>
-      </div>
+        }
+      }>Decrease Counter By One</button>
+    </div>
   );
 }
-export default CounterButton;
+
+function mapDispatchToProps(dispatch) {
+  return {
+    increase: function () {
+      const action = increaseCounter();
+      dispatch(action);
+    },
+    decrease: function () {
+      const action = decreaseCounter();
+      dispatch(action);
+    }
+  }
+}
+
+export default connect(mapDispatchToProps)(CounterButton);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,28 +1,33 @@
+import { combineReducers } from 'redux';
 
-function currentCount(state=0, action){
-  if(action.type === "INCREASE_COUNTER"){
-    
+function currentCount(state = 0, action) {
+  if (action.type === "INCREASE_COUNTER") {
+    return state + 1;
   }
-  if(action.type === "DECREASE_COUNTER"){
-    
+  if (action.type === "DECREASE_COUNTER") {
+    return state - 1;
   }
   return state;
-}   
+}
 
-function users(state =[], action){
-  if(action.type === "ADD_USER"){
+function users(state = [], action) {
+  if (action.type === "ADD_USER") {
 
   }
-  if(action.type === "REMOVE_USER"){
-    
+  if (action.type === "REMOVE_USER") {
+
   }
   return state;
 }
 
 
-function specialText(state = "", action){
-  if(action.type === "SET_SPECIAL_TEXT"){
+function specialText(state = "", action) {
+  if (action.type === "SET_SPECIAL_TEXT") {
     return action.value;
   }
   return state;
 }
+
+export default combineReducers({
+  currentCount, users, specialText
+})
