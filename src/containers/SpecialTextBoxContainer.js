b96d0cff9137f4a7d3108b5a31fd928c306ed893
cynthiawilliamsa <cynthiawilliamsa@gmail.com>
2018-12-06 19:05:18 -0600

    initial commit text box and text display done

--- src/containers/SpecialTextBoxContainer.js
+++ src/containers/SpecialTextBoxContainer.js
@@ -2,14 +2,13 @@ import { connect } from 'react-redux';
 import {setSpecialText} from "../actions";
 import SpecialTextBox from "../components/SpecialTextBox";
 
-
-function mapDispatchToProps(dispatch){
+function mapDispatchToProps(dispatch) {
   return {
-   set:function(txt){
-     let action = setSpecialText(txt);
-     dispatch(action);
-   }
+    set: function(txt){
+    var action = setSpecialText(txt);
+    dispatch(action);
+    }
   }
 }
 
-export default connect(null,mapDispatchToProps)(SpecialTextBoxContainer);
+export default connect(null,mapDispatchToProps)(SpecialTextBox);
--- src/containers/SpecialTextContainer.js
+++ src/containers/SpecialTextContainer.js
@@ -8,4 +8,4 @@ function mapStateToProps(state){
   }
 }
 
-export default connect(mapStateToProps)(SpecialTextContainer);
+export default connect(mapStateToProps)(SpecialText);
