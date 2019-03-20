491b51601ece20b08c46af06326627310457f94b
Abby Gottlich <abby.gottlich@gmail.com>
2019-03-20 17:28:20 -0500

    basic reducer functions created

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -38,8 +38,64 @@ function specialText(state = "", action) {
   return state;
 }
 
+function currentCity(state = "", action) {
+  if (action.type === "SET_CURRENT_CITY") {
+    return action.value;
+  }
+  return state;
+}
+
+function searchText(state = "", action) {
+  if (action.type === "SEARCH_TEXT") {
+    return action.value;
+  }
+  return state;
+}
+
+function currentTemp(state = "", action) {
+  if (action.type === "SET_CURRENT_TEMP") {
+    return action.value;
+  }
+  return state;
+}
+
+function isLoading(state = "", action) {
+  if (action.type === "IS_LOADING") {
+    return action.value;
+  }
+  if (action.type === "IS_NOT_LOADING") {
+    return action.value;
+  }
+  return state;
+}
+
+function videoURL(state = "", action) {
+  if (action.type === "SET_VIDEO_URL") {
+    return action.value;
+  }
+  return state;
+}
+
+function currentUserSort(state = "", action) {
+  if (action.type === "FIRST_NAME") {
+    return action.value;
+  }
+  if (action.type === "LAST_NAME") {
+    return action.value;
+  }
+  return state;
+}
+
+function videoScale(state = "", action) {
+  if (action.type === "SET_VIDEO_SIZE") {
+    return action.value;
+  }
+  return state;
+}
+
 let reducers = combineReducers({
-  currentCount, users, specialText
+  currentCount, users, specialText, currentCity, searchText, currentTemp,
+  isLoading, videoURL, currentUserSort, videoScale
 })
 
 export default reducers;
