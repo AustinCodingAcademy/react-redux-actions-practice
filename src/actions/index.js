14de90d09bf3ed89e2d77c5e2f0eb09b098ce138
Landon Taylor <landon.autumn.taylor@gmail.com>
2019-08-28 20:03:12 -0500

    actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,79 +1,92 @@
+export const INCREASE_COUNTER = "INCREASE_COUNTER";
 export function increaseCounter(){
   return {
-    type:"INCREASE_COUNTER"
+    type:INCREASE_COUNTER
   }
 }
 
+export const DECREASE_COUNTER = "DECREASE_COUNTER";
 export function decreaseCounter() {
   return {
-    type:"DECREASE_COUNTER"
+    type:DECREASE_COUNTER
   }
 }
 
+export const SET_SPECIAL_TEXT = "SET_SPECIAL_TEXT";
 export function setSpecialText(txt){
   return {
-    type:"SET_SPECIAL_TEXT",
+    type:SET_SPECIAL_TEXT,
     value:txt
   }
 }
 
+export const ADD_USER = "ADD_USER";
 export function addUser(user){
   return {
-    type:"ADD_USER",
+    type:ADD_USER,
     value:user
   }
 }
+
+export const REMOVE_USER = "REMOVE_USER";
 export function removeUser(){
   return {
-    type:"REMOVE_USER"
+    type:REMOVE_USER
   }
 }
 
+export const SET_SEARCH_TEXT = "SET_SEARCH_TEXT";
 export function setSearchText(text) {
   return {
-    type:"SET_SEARCH_TEXT",
+    type:SET_SEARCH_TEXT,
     value:text
   }
 }
 
+export const SET_IS_LOADING = "SET_IS_LOADING";
 export function setIsLoading(isLoading) {
   return {
-    type:"SET_IS_LOADING",
+    type:SET_IS_LOADING,
     value:isLoading
   }
 }
 
+export const SET_TEMP = "SET_TEMP";
 export function setTemp(temp) {
   return {
-    type:"SET_TEMP",
+    type:SET_TEMP,
     value:temp
   }
 }
 
+export const SET_CURRENT_CITY = "SET_CURRENT_CITY";
 export function setCurrentCity(city) {
   return {
-    type:"SET_CURRENT_CITY",
+    type:SET_CURRENT_CITY,
     value:city
   }
 }
 
+export const SET_VIDEO_URL = "SET_VIDEO_URL";
 export function setVideoURL(URL) {
   return {
-    type:"SET_VIDEO_URL",
+    type:SET_VIDEO_URL,
     value:URL
   }
 }
 
+export const SET_CURRENT_USER_SORT = "SET_CURRENT_USER_SORT";
 export function setCurrentUserSort(sort) {
   return {
-    type:"SET_CURRENT_USER_SORT",
+    type:SET_CURRENT_USER_SORT,
     value:sort
   }
 }
 
+export const SET_VIDEO_SCALE = "SET_VIDEO_SCALE";
 export function setVideoScale(scale) {
   return {
-    type:"SET_VIDEO_SCALE",
+    type:SET_VIDEO_SCALE,
     value: scale
   }
 }
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,80 +1,93 @@
 import {combineReducers} from 'redux';
+import {INCREASE_COUNTER} from '../actions/index';
+import {DECREASE_COUNTER} from '../actions/index';
+import {SET_SPECIAL_TEXT} from '../actions/index';
+// import {LOAD_USERS} from '../actions/index';
+import {ADD_USER} from '../actions/index';
+import {REMOVE_USER} from '../actions/index';
+import {SET_CURRENT_CITY} from '../actions/index';
+import {SET_SEARCH_TEXT} from '../actions/index';
+import {SET_TEMP} from '../actions/index';
+import {SET_IS_LOADING} from '../actions/index';
+import {SET_VIDEO_URL} from '../actions/index';
+import {SET_CURRENT_USER_SORT} from '../actions/index';
+import {SET_VIDEO_SCALE} from '../actions/index';
 
 function currentCount(state=0, action){
-  if(action.type === "INCREASE_COUNTER"){
+  if(action.type === INCREASE_COUNTER){
     return state +1
   }
-  if(action.type === "DECREASE_COUNTER"){
+  if(action.type === DECREASE_COUNTER){
     return state -1
   }
   return state;
 }
 
 function users(state =[], action){
-  if(action.type === 'LOAD_USERS') {
-    return [...state, action.value]
-  }
+  // if(action.type === LOAD_USERS) {
+  //   return [...state, action.value]
+  // }
 
-  if(action.type === "ADD_USER"){
+  if(action.type === ADD_USER){
     return [...state,action.value]
   }
-  if(action.type === "REMOVE_USER"){
+  if(action.type === REMOVE_USER){
     return state.slice(1)
   }
   return state;
 }
 
 function specialText(state = "", action){
-  if(action.type === "SET_SPECIAL_TEXT"){
+  if(action.type === SET_SPECIAL_TEXT){
     return action.value;
   }
   return state;
 }
 
 function currentCity(state="",action){
-  if(action.type === "SET_CURRENT_CITY") {
+  if(action.type === SET_CURRENT_CITY) {
     return action.value;
   }
   return state;
 }
 
 function searchText(state = "", action){
-  if(action.type === "SET_SEARCH_TEXT"){
+  if(action.type === SET_SEARCH_TEXT){
     return action.value;
   }
   return state;
 }
 
 function currentTemp(state=0,action){
-  if(action.type==="SET_TEMP") {
+  if(action.type===SET_TEMP) {
     return action.value;
   }
   return state;
 }
 
 function isLoading(state=false,action){
-  if(action.type==="SET_IS_LOADING") {
+  if(action.type===SET_IS_LOADING) {
     return action.value;
   }
   return state;
 }
 
 function videoURL(state="",action){
-  if(action.type==="SET_VIDEO_URL") {
+  if(action.type===SET_VIDEO_URL) {
     return action.value;
   }
   return state;
 }
 
 function currentUserSort(state="first_name",action){
-  if(action.type==="SET_CURRENT_USER_SORT") {
+  if(action.type===SET_CURRENT_USER_SORT) {
     return action.value;
   }
   return state;
 }
 
 function videoScale(state=1,action){
-  if(action.type==="SET_VIDEO_SCALE") {
+  if(action.type===SET_VIDEO_SCALE) {
     return action.value;
   }
   return state;
